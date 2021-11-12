38de6f8e620b112f6cf04a38874ca478e25ed0db
xbfu <fxb163@163.com>
2021-11-11 19:50:41 -0500

    Update public-asgd.py

--- public-asgd.py
+++ public-asgd.py
@@ -107,7 +107,7 @@ def main():
     parser.add_argument("--model", type=str, default="resnet18", help="The job's name.")
     parser.add_argument("--rank", type=int, default=1, help="Global rank of this process.")
     parser.add_argument("--world_size", type=int, default=3, help="Total number of workers.")
-    parser.add_argument("--data_dir", type=str, default="./data/imagenette2/val", help="The location of dataset.")
+    parser.add_argument("--data_dir", type=str, default="./imagenette2/val", help="The location of dataset.")
     parser.add_argument("--master_addr", type=str, default="localhost", help="Address of master.")
     parser.add_argument("--master_port", type=str, default="29600", help="Port that master is listening on.")
     parser.add_argument("--batch_size", type=int, default=64, help="Batch size of each worker during training.")
